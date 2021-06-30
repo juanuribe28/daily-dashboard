d0eddb3486cda5aa05ac74208bbabcc7a8d856c5
JuanJUribe <juanjouribev1@gmail.com>
2021-06-29 17:42:05 -0700

    Add weather widget

---
+++ scripts/weather.js
@@ -0,0 +1,18 @@
+const LAT = 33.97001973022271;
+const LON = -118.41454892413182;
+// For docs on the API response check https://openweathermap.org/api/one-call-api#how
+const BASE_API_URL = 'https://api.openweathermap.org/data/2.5/onecall';
+
+const PARAMS = {lat: LAT,
+                lon: LON,
+                appid: API_KEYS.weather,
+                units: 'imperial'};
+
+const API_URL = makeRequestUrl(BASE_API_URL, PARAMS);
+const TEST_URL = "/test_data/weather.json";
+
+const JSON_PROMISE = loadJSON(TEST_URL)
+
+const TEMPLATE_PROMISE = loadTemplate('/templates/weather.html')
+
+loadComponentToDashboard(TEMPLATE_PROMISE, JSON_PROMISE)
