0e325322acaaafb03dcc73c6217995fce7908455
JuanJUribe <juanjouribev1@gmail.com>
2021-06-30 14:12:05 -0700

    Fix style to use 2 spaces

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,6 +1,6 @@
 function loadTemplate(url) {
-    const templatePromise = fetch(url).then(promiseResponse => promiseResponse.text()).catch(error => console.log(error));
-    return templatePromise;
+  const templatePromise = fetch(url).then(promiseResponse => promiseResponse.text()).catch(error => console.log(error));
+  return templatePromise;
 }
 
 function loadJSON(url) {
@@ -9,14 +9,19 @@ function loadJSON(url) {
 }
 
 function makeRequestUrl(base_url, params) {
-    const queryStr = Object.keys(params)
-                           .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-                           .join('&');
-    return `${base_url}?${queryStr}`;
+  const queryStr = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    .join('&');
+  return `${base_url}?${queryStr}`;
 }
 
-async function loadComponentToDashboard(templatePromise, jsonPromise) {
-    const results = await Promise.all([templatePromise, jsonPromise])
-    let rendered = Mustache.render(results[0], results[1]);
-    $('#dashboard').append(rendered)
+async function loadComponentToDashboard(templatePromise, jsonPromise, jsonPreprocess = nothing) {
+  const results = await Promise.all([templatePromise, jsonPromise]);
+  let rendered = Mustache.render(results[0], jsonPreprocess(results[1]));
+  $('#dashboard').append(rendered);
+  return true;
+}
+
+function nothing(input) {
+  return input;
 }
